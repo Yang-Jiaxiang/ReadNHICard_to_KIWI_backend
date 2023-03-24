9913997070e6839a3a3d144617975cd877c95788
Louis <81738019+Yang-Jiaxiang@users.noreply.github.com>
2023-03-24 17:00:37 +0800

    fix:修正登入視窗關閉程式無終止問題
    修改為開啟程式後Main 與 Login 視窗都開啟，讀卡時確認是否登入，若無登入則跳登入視窗，

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 from datetime import datetime
 import pprint
-import DBControl
 import script.cardReader
 from PIL import Image
 from pystray import MenuItem as item
@@ -14,17 +13,21 @@ import json
 from tkinter import messagebox
 
 load_dotenv()
-AUTH_API=os.getenv("AUTH_API")
-AUTH_ADDPATIENTAPI=os.getenv("AUTH_ADDPATIENTAPI")
+BACK_END_URL=os.getenv("BACK_END_URL")
 
 api_token=""
-class LoginWindow:
-    def __init__(self, parent):
-        self.parent = parent
+isLoging = False
 
+class LoginWindow:
+
+    def __init__(self, parent):
+
+        self.parent = parent
         self.login_window = tk.Toplevel(parent)  # 創建登錄視窗
         self.login_window.title("登錄")  # 設置登錄視窗的標題
         self.login_window.geometry("200x80")
+        self.login_window.resizable(0, 0)
+
         tmp = open("tmp.ico", "wb+")
         tmp.write(base64.b64decode(img))
         tmp.close()
@@ -50,16 +53,20 @@ class LoginWindow:
 
         self.login_button.grid(row=2, column=1, columnspan=2)
         self.session = requests.Session()
+
+
     def login(self):
         global api_token
+        global isLoging
         # 在這裡添加您的登錄邏輯，例如驗證用戶名和密碼是否正確
         # 如果登錄成功，則關閉登錄窗口並顯示主窗口
-        userName=self.username_entry.get()
-        passWork=self.password_entry.get()
-        url = AUTH_API
-        myobj = {'username': userName,"password":passWork}
+        userName = self.username_entry.get()
+        passWork = self.password_entry.get()
+        url = BACK_END_URL + "/auth/login"
+        myobj = {'username': userName, "password": passWork}
         x = requests.post(url, json=myobj)
-        if(x.status_code==200):
+        if (x.status_code == 200):
+            isLoging=True
             api_token = json.loads(x.text)['token']
             self.login_window.destroy()
             self.parent.deiconify()
@@ -67,6 +74,7 @@ class LoginWindow:
             messagebox.showerror('登入錯誤', json.loads(x.text)['message'])
 
 class MainWindow:
+
     def __init__(self):
         self.root = tk.Tk()
         self.root.geometry('300x400')
@@ -77,80 +85,46 @@ class MainWindow:
         tmp.close()
         self.root.iconbitmap("tmp.ico")
         os.remove("tmp.ico")
+        global isLoging
 
         # 創建一個登錄視窗實例，但是不顯示它
         self.login = LoginWindow(self.root)
-
-        # 在主窗口顯示前，先隱藏主窗口
-        self.root.withdraw()
-
-        # 在主窗口加載時顯示登錄視窗
-        self.root.after(0, self.login.login_window.lift)
-        self.root.after(0, self.login.login_window.focus_force)
-
-        def quit_window(icon, item):
-            icon.stop()
-            self.root.destroy()
-            self.root.quit()
-
-        def show_window(icon, item):
-            icon.stop()
-            self.root.after(0, self.root.deiconify)
-
-        def withdraw_window():
-            self.root.withdraw()
-            image = Image.open("logo.ico")
-            menu = (item('Quit', quit_window), item('Show', show_window))
-            icon = pystray.Icon("name", image, "title", menu)
-            icon.run()
-
-        # 視窗關閉
-        self.root.protocol('WM_DELETE_WINDOW', withdraw_window)
 
         def add_Log_Message(message):
             now = datetime.now()
             dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
             logValue.insert(tk.INSERT, dt_string + ":" + "\n" + message + "\n=============================\n")
 
-        def Check_DB_Button():
-            if DBControl.Check_DB_Onnection() == 200:
-                add_Log_Message("資料庫連線成功")
-            else:
-                add_Log_Message("資料庫連線失敗！！！")
-
         def readCard_button_event():
-            cardContent = script.cardReader.readCard()
-            carCode = str(cardContent)[9:12]
-            if carCode == "500":
-                add_Log_Message("尚未讀取到卡片")
-            if carCode == "200":
-                add_Log_Message("讀取卡片成功")
-                pretty_print_json = pprint.pformat(cardContent).replace("'", '"')
-                url = AUTH_ADDPATIENTAPI
-                cookies = {'accessToken': api_token}
-                x = requests.post(url, cookies=cookies, json=cardContent)
-                if(x.status_code==200):
-                    add_Log_Message("新增資料庫成功")
-                else:
-                    if("E11000" in json.loads(x.text)["message"]):
-                         add_Log_Message("已存在資料庫")
+            if(isLoging==False):
+                self.login = LoginWindow(self.root)
+            else:
+                cardContent = script.cardReader.readCard()
+                carCode = str(cardContent)[9:12]
+                if carCode == "500":
+                    add_Log_Message("尚未讀取到卡片")
+                if carCode == "200":
+                    add_Log_Message("讀取卡片成功")
+                    pretty_print_json = pprint.pformat(cardContent).replace("'", '"')
+                    url = BACK_END_URL+"/api/patient"
+                    cookies = {'accessToken': api_token}
+                    x = requests.post(url, cookies=cookies, json=cardContent)
+                    if(x.status_code==200):
+                        add_Log_Message("新增資料庫成功")
                     else:
-                        add_Log_Message("其他錯誤，請重啟讀卡程式")
-
-                #     print ('已存在資料庫')
-                # DB_insertState = DBControl.Insert_DB(cardContent)
-                #
-                # if DB_insertState == 200:
-                #     add_Log_Message("新增資料庫成功")
-                # if DB_insertState == 500:
-                #     add_Log_Message("新增資料庫失敗")
-
-        # 檢查資料庫連線
-        Check_DB_button = tk.Button(self.root, text='檢查連線資料庫連線', command=Check_DB_Button)
-        Check_DB_button.pack()
+                        if("E11000" in json.loads(x.text)["message"]):
+                             add_Log_Message("已存在資料庫")
+                        else:
+                            add_Log_Message("其他錯誤，請重啟讀卡程式")
 
         # 讀取卡片
-        cardReadbutton = tk.Button(self.root, text='讀取卡片/手動加入病患', command=readCard_button_event)
+        cardReadbutton = tk.Button(self.root,
+                                    text='讀取卡片/加入病患',
+                                    font=('Arial',18,'bold'),
+                                    padx=10,
+                                    pady=10,
+                                    activeforeground='#f00',
+                                    command=readCard_button_event)
         cardReadbutton.pack()
 
         # 下方log message
@@ -159,5 +133,5 @@ class MainWindow:
 
         self.root.mainloop()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = MainWindow()
